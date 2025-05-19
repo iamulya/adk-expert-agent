3da44c4644aed89c0526ef5cffba999919dda3c4
Amulya Bhatia <amulya.bhatia@t-online.de>
2025-05-19 13:00:02 +0000

    change default model because of pricing issues

--- expert-agents/config.py
+++ expert-agents/config.py
@@ -2,4 +2,4 @@
 # Centralized configuration for the ADK Expert Agent
 
 # The default model name to be used by agents and tools
-DEFAULT_MODEL_NAME = "gemini-2.5-pro-preview-05-06"
+DEFAULT_MODEL_NAME = "gemini-2.5-flash-preview-04-17"#"gemini-2.5-pro-preview-05-06"
